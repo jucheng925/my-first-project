2189496a8ffb7644caad5232b17591fe50a04bfd
Julie C <jucheng925@gmail.com>
2023-02-24 13:02:59 -0500

    include the block and javascript movement

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+//const h2 = document.createElement("h2");
+//h2.textContent = "This content added by JavaScript";
+//document.querySelector("body").appendChild(h2);
+
+const block = document.getElementById("block");
+function moveBlockLeft() {
+    const leftNumbers = block.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      block.style.left = `${left - 5}px`;
+    }
+  }
+
+function moveBlockRight() {
+    const leftNumbers = block.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    if (left < 420) {
+        block.style.left = `${left + 5}px`;
+    }
+}
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+        moveBlockLeft();
+    }
+    if (e.key === "ArrowRight") {
+        moveBlockRight();
+    }
+});

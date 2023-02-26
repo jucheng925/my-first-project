206da20149f56e06a6233aa4213243518fe24321
Julie C <jucheng925@gmail.com>
2023-02-26 15:05:36 -0500

    adding the first movement to the ball

--- script.js
+++ script.js
@@ -20,11 +20,26 @@ function moveBlockRight() {
     }
 }
 
+
 document.addEventListener("keydown", function (e) {
     if (e.key === "ArrowLeft") {
-        moveBlockLeft();
+        //moveBlockLeft();//
+        startBall();
     }
     if (e.key === "ArrowRight") {
         moveBlockRight();
     }
 });
+
+
+function startBall() {
+    const leftBallNumbers = ball.style.left.replace("px", "");
+    const bottomBallNumbers = ball.style.bottom.replace ("px","");
+    const leftBall = parseInt(leftBallNumbers, 10);
+    const bottomBall = parseInt(bottomBallNumbers, 10);
+    if (leftBall > 0) {
+        ball.style.left = `${leftBall - 5}px`
+        ball.style.bottom = `${bottomBall + 5}px`
+    }
+
+}
